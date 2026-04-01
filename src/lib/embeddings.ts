ebb3c963a5359bf001fa7cb7e2436afb207e4411
Ruchir <ruchir.dhanawade@gmail.com>
2026-04-01 22:55:26 +0530

    Server issues resolved and vector search implemented

--- src/lib/embeddings.ts
+++ src/lib/embeddings.ts
@@ -1,15 +1,25 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-const model = genAI.getGenerativeModel({model: "text-embedding-004"})
-
+const ai = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_API_KEY!,
+});
 
 export async function getEmbeddings(text: string): Promise<number[]> {
   try {
-    const result = await model.embedContent(text);
-    return result.embedding.values; // Already float[] vector
+    const response = await ai.models.embedContent({
+      model: "gemini-embedding-001",
+      contents: text,
+    });
+
+    // SAFE extraction
+    const embed = response.embeddings?.[0]?.values;
+    if (!embed) {
+      throw new Error("Failed to generate embeddings");
+    }
+
+    return embed;
   } catch (err) {
     console.error("Gemini Embedding Error:", err);
     throw err;
   }
-};
+}
